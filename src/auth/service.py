c48b3da155ca58e5d79578d3c6ca603fd0926c7f
Oladapo Lawore <dapolawore@gmail.com>
2024-08-09 09:32:48 +0100

    ft: user registration and login

--- src/auth/service.py
+++ src/auth/service.py
@@ -8,6 +8,7 @@ from .schemas import UserCreate
 
 from .models import User
 
+# used for hashing password
 bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="author")
 oauth2_bearer = OAuth2PasswordBearer(tokenUrl="/v1/auth/token")
 SECRET_KEY = "secretkey"
@@ -44,19 +45,31 @@ async def get_current_user(db: Session, token: str = Depends(oauth2_bearer)):
         if username is None or id is None:
             return None
 
-        return db.query(User).filter(User.id = id).first()
+        return db.query(User).filter(User.id=id).first()
     except JWTError:
         return None
 
 
 # get user from user id
-async def get_user_from_user_id(db:Session, user_id:int ):
-    return db.query(User).filter(User.id = user_id).first()
+async def get_user_from_user_id(db: Session, user_id: int):
+    return db.query(User).filter(User.id=user_id).first()
 
 
-#def create user
-async def create_user(db:Session, user: UserCreate):
-    pass
+# def create user
+async def create_user(db: Session, user: UserCreate):
+    db_user = User(email=user.email.lower().strip(), username=user.username.lower().strip(), hashed_password=bcrypt_context.hash(
+        user.password), db=user.db or None, gender=user.gender or None, 
+        bio = user.bio or None, profile_pic=user.profile_pic or None, name = user.name or None)
+    db.add(db_user)
+    db.commit()
+    return db_user
 
 
-
+# authenticate
+async def authenticate(db:Session, username:str, password: str):
+    db_user = await existing_user(db, username, "")
+    if not db_user:
+        return None
+    if not bcrypt_context.verify(password, db_user.password):
+        return None
+    return db_user
--- src/auth/views.py
+++ src/auth/views.py
@@ -4,7 +4,8 @@ from sqlalchemy.orm import Session
 from datetime import datetime
 from .schemas import UserCreate, UserUpdate, User as UserSchema
 from ..database import get_db
-from .service import existing_user, create_access_token, get_current_user
+from .service import existing_user, create_access_token, get_current_user,create_user as create_use_svc, authenticate
+
 
 router = APIRouter(prefix="/auth", tags=["auth"])
 # apis
@@ -16,9 +17,24 @@ async def create_user(user: UserCreate, db: Session = Depends(get_db))
     if db_user:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
                             detail="Username already in use")
-     
+    db_user = create_use_svc(db, user)
+    access_token = create_access_token(user.username, db_user.id)
+    return  {
+        "access_token": access_token,
+        "token_type": "bearer",
+        "username" : user.name
+    }
 
 # login to generate token
+@router.post('/token', status_code=status.HTTP_200_OK)
+async def login(formData: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+    db_user = await authenticate(db, formData.username, formData.password)
+    if not db_user:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail= "Incorrect username  or password")
+    
+    access_token = await create_access_token(db_user.username, db_user.id )
+    return {"access_token": access_token, "token_type": "bearer",}
+
 # get user
 # update user
 # reset password
